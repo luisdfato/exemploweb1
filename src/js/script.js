ce234adfacd463018f434d4a41cba018ca0e2e56
Luis <luisfernandolemecampos2007@gmail.com>
2026-03-30 09:15:46 -0300

    feat: Declarações e variaveis

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,47 @@
-alert("Hello Word !!!")
+//Declarações e variaveis 
+//var , let e const
+
+var nome="Fiap"; 
+console.log(nome); 
+
+var nome="Teste";
+
+if(true){
+    let apelido="Fiapinho"
+    console.log(apelido);
+}
+
+let tavaJuros=0.05;
+console.log(taxaJuros); 
+// taxaJuros=1.5
+
+var exemplo="ola-dev-var";
+console.log( typeof exemplo)
+
+let exemplo="ola-dev-var";
+console.log(exemplo1)="ola-dev-let";
+
+const exemplo="ola-dev-const";
+console.log(exemplo2)="ola-dev-const";
+
+let exemplo3=10;
+console.log(exemplo3)
+
+let exemplo4=true; 
+console.log(exemplo4);
+
+let exemplo5={nome1 : "teste"};
+console.log(exemplo5);
+
+let exemplo6={nome: "teste"}
+console.log(exemplo6);
+
+let exemplo=("Java","C#","PHP")
+
+//valor undefined 
+let exemplo7;
+console.log(exemplo7)
+
+//valor vazio
+let exemplo8 =null;
+console.log(exemplo8)
