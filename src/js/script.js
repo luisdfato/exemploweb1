46dcf06cbc2ef83c619ea627eefd522fef4a65f0
Luis <luisfernandolemecampos2007@gmail.com>
2026-04-06 09:11:15 -0300

    feat:Estrutura condicionais

--- src/js/script.js
+++ src/js/script.js
@@ -1,68 +1,130 @@
-//Declarações e variaveis 
-//var , let e const
+// //Declarações e variaveis 
+// //var , let e const
 
-var nome="Fiap"; 
-console.log(nome); 
+// var nome="Fiap"; 
+// console.log(nome); 
 
-var nome="Teste";
+// var nome="Teste";
 
-if(true){
-    let apelido="Fiapinho"
-    console.log(apelido);
+// if(true){
+//     let apelido="Fiapinho"
+//     console.log(apelido);
+// }
+
+// const tavajuros=0.05;
+// console.log(taxajuros); 
+
+// var exemplo="ola-dev-var";
+// console.log( typeof exemplo)
+
+// let exemplo2="ola-dev-var";
+// console.log(exemplo2)="ola-dev-let";
+
+// const exemplo3="ola-dev-const";
+// console.log(exemplo3)="ola-dev-const";
+
+// let exemplo4=10;
+// console.log(exemplo4)
+
+// let exemplo5=true; 
+// console.log(exemplo4);
+
+// let exemplo6={nome1 : "teste"};
+// console.log(exemplo5);
+
+// let exemplo7={nome: "teste"}
+// console.log(exemplo6);
+
+// let exemplo=("Java","C#","PHP")
+
+// //valor undefined 
+// let exemplo8;
+// console.log(exemplo7)
+
+// //valor vazio
+// let exemplo9 =null;
+// console.log(exemplo8)
+
+// // OPERADORES ARITIMETICOS
+
+// const valor1=10;
+// const valor2=20;
+
+// console.log( valor1 + valor2);
+// console.log( valor1 - valor2);
+// console.log( valor1 * valor2);
+// console.log( valor1 / valor2);
+
+// //OPERADORES LOGICOS 
+// // = atribuições 
+// // == comparação 
+// // === valor e tipo(estritante igual)
+// // && = E (todas condições tem que ser verdadeiras)
+// // || = ou (uma das condições tem que ser verdadeiras)
+
+// console.log(valor1 < valor2);
+// console.log( valor1 > 10 && valor2 < 10 );
+// console.log( valor2 > 100 || valor1 < 10 );
+// console.log( valor1 > 10 ==valor2);
+
+// estrutura condicional 
+
+//if 
+
+
+let nome ="fiap";
+
+if(nome =="fiap"){
+    console.log("nome correto")
 }
 
-const tavajuros=0.05;
-console.log(taxajuros); 
+// if/else
 
-var exemplo="ola-dev-var";
-console.log( typeof exemplo)
+if(nome ="fiap"){
+    console.log("usuario Correto")
+}else{
+    console.log("usuario Errado")
+}
 
-let exemplo2="ola-dev-var";
-console.log(exemplo2)="ola-dev-let";
+// if encadeado alinhado 
 
-const exemplo3="ola-dev-const";
-console.log(exemplo3)="ola-dev-const";
+let idade = 17; 
 
-let exemplo4=10;
-console.log(exemplo4)
+if(idade <= 13){
+    conmsole.log("é uma criança")
+}else if (idade >13 && idade <=18){
+    console.log("é um adolecente")
+}else if(idade >18 && idade <50)
+    console.log("é um adulto")
+else{
+console.log("é um idoso")
+}
 
-let exemplo4=true; 
-console.log(exemplo4);
+// switch case 
 
-let exemplo5={nome1 : "teste"};
-console.log(exemplo5);
+let pratos="salada";
 
-let exemplo6={nome: "teste"}
-console.log(exemplo6);
+switch(pratos){
+    case "macarrao":
+    console.log("a melhor massa")
+    break;
+    case "salada":
+    console.log("salada saudadvel")
+    break;
+    case "lasanah":
+        console.log("melhor prato")
+        break;
+        default:
+            console.log("nenhuma das opções ")
+}
 
-let exemplo=("Java","C#","PHP")
+// ternario 
 
-//valor undefined 
-let exemplo7;
-console.log(exemplo7)
+let valor =100 
+let resultado = valor ==100 ? "Valor certo": "Valor errado";
+console.log(resultado)
 
-//valor vazio
-let exemplo8 =null;
-console.log(exemplo8)
-
-// OPERADORES ARITIMETICOS
-
-const valor1=10;
-const valor2=20;
-
-console.log( valor1 + valor2);
-console.log( valor1 - valor2);
-console.log( valor1 * valor2);
-console.log( valor1 / valor2);
-
-//OPERADORES LOGICOS 
-// = atribuições 
-// == comparação 
-// === valor e tiopo(estritante igual)
-// && = E (todas condições tem que ser verdadeiras)
-// || = ou (uma das condições tem que ser verdadeiras)
-
-console.log(valor1 < valor2);
-console.log( valor1 > 10 && valor2 < 10 );
-console.log( valor2 > 100 || valor1 < 10 );
-console.log( valor1 > 10 ==valor2);
+let usuario="dev";
+let logado= usuario =="dev" ? "usuario logado": "usuario invalido";
+console.log(logado)
+    
